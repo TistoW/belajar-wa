3e3dcab9465299207663bd4f7a9413e97d6014cb
tisto <imtisto@gmail.com>
2024-11-01 19:41:38 +0700

    - init

---
+++ app/src/main/java/com/robeth/whatsapp/FragmentKomunitasActivity.kt
@@ -0,0 +1,13 @@
+package com.robeth.whatsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FragmentKomunitasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_komunitas)
+
+
+    }
+}
---
+++ app/src/main/java/com/robeth/whatsapp/buttonActivity.kt
@@ -0,0 +1,41 @@
+package com.robeth.whatsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class buttonActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.button_bar_bottom)
+
+        val btn1= findViewById<LinearLayout>(R.id.LLchat)
+        val btn2= findViewById<LinearLayout>(R.id.LLpembaruan)
+        val btn3= findViewById<LinearLayout>(R.id.LLkomunitas)
+        val btn4= findViewById<LinearLayout>(R.id.LLpanggilan)
+
+        btn1.setOnClickListener{
+            intent = Intent(this, FragmentChatActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        btn2.setOnClickListener{
+            intent = Intent(this, FragmentPembaruanActivity::class.java).also {
+                startActivity(it)
+            }
+
+            }
+        btn3.setOnClickListener{
+            intent = Intent(this, FragmentKomunitasActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        btn4.setOnClickListener{
+            intent = Intent(this, FragmentPanggilanActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+    }
+}
